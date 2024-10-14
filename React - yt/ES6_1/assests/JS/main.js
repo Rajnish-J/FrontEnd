d162ad95bfedd67c4f5426bea4c36a96d5c0ef33
Rajnish-J <rajnishofficial02@gmail.com>
2024-10-14 19:52:31 +0530

    new DOM practice started Old files deleted

--- React - yt/ES6_1/assests/JS/main.js
+++ React - yt/ES6_1/assests/JS/main.js
@@ -4,15 +4,15 @@
 //     console.log(i);
 // }
 
-console.log(i); // * after the loop iteration 'i' value incremented
+// console.log(i); // * after the loop iteration 'i' value incremented
 
 // ! let => if we use let that only allows in the functional block level code
 
-for(let i = 1; i<= 5; i++){
-    console.log(i);
-}
+// for(let i = 1; i<= 5; i++){
+//     console.log(i);
+// }
 
-console.log(i); // * gives undefined
+// console.log(i); // * gives undefined
 
 // ! const => we cant change the values in the const data type, we can add the values (push method) like array, but for the added value we can't modify the values
 
@@ -21,3 +21,14 @@ console.log(i); // * gives undefined
 // ! If you use var inside of a function, it belongs to that function.
 // ! If you use var inside of a block, i.e. a for loop, the variable is stilt available outside of thot block.
 
+var a = 18;
+
+function overideA(){
+    var a = 7;
+    console.log("Local " + a);
+    
+}
+
+console.log("global " + a);
+overideA();
+console.log("After calling method: "  + a);
